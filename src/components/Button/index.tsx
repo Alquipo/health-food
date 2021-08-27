f4487c017d4508dc076de62c41efcb849f788587
Alquipo <alquiponeto@outlook.com.br>
2021-08-27 03:44:43 -0300

    feat: Button Component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,22 @@
+import { HTMLAttributes } from 'react'
+import * as S from './styles'
+
+type ButtonTypes = HTMLAttributes<HTMLButtonElement>
+
+export type ButtonProps = {
+  type?: 'button' | 'submit' | 'reset' | undefined
+  boxShadow?: boolean
+} & ButtonTypes
+
+export default function Button({
+  type = 'button',
+  children,
+  boxShadow,
+  ...props
+}: ButtonProps) {
+  return (
+    <S.Wrapper {...props} type={type} boxShadow={boxShadow}>
+      {children}
+    </S.Wrapper>
+  )
+}
