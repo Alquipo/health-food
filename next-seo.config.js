50837c4609da23acc5254826ee00086a56a15710
Alquipo <alquiponeto@outlook.com.br>
2021-08-28 16:41:26 -0300

    feat: SEO dependencie

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://health-food.alquipo.dev/',
+    site_name: 'Healthy Food',
+    title: 'Healthy Food'
+  },
+  twitter: {
+    handle: '@alquiponeto',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
